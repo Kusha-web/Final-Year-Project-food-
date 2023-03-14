933e5b950c3fb014ec3425c34e4524832452bef5
Kusha-web <kushalmitra189@gmail.com>
2023-03-14 09:44:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { colorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from'./components/global/Topbar';
+import SideBar from './components/global/SideBar';
+import Order from './components/Order';
+import CustomizeFood from "./components/CustomizeFood";
+import { Route, Routes } from "react-router-dom";
+import StepContext from "./components/CustomizeFood/stepContext";
+import VendorProfile from "./components/VendorProfile";
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <colorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app">
+          <SideBar/>
+          <main className="content" style={{marginLeft: "300px"}}>
+            <Topbar/>
+            <Routes>
+            <Route path="/order" element={<Order />} />
+            <Route path="/vendorprofile" element={<VendorProfile/>} />
+            <Route path="/customizefood" element={<StepContext><CustomizeFood /></StepContext>} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </colorModeContext.Provider>
   );
 }
 
